4c4250141d19224653a0756ba17521c46f9fd6d8
Balint Bakos <balint.bb@gmail.com>
2020-04-13 17:45:30 +0200

    docs fade in fade out

--- public/js/app.js
+++ public/js/app.js
@@ -3,5 +3,15 @@ console.log('JS is running...')
 const showDetails = (type) => {
     const hidden = document.getElementById(`hidden-${type}`)
     console.log(hidden)
-    hidden.hidden = !hidden.hidden
+    if (hidden.hidden) {
+      $(`#hidden-${type}`).fadeIn('slow')
+      hidden.hidden = !hidden.hidden
+      return
+    }
+    
+    $(`#hidden-${type}`).fadeOut('slow')
+    setTimeout(() => {
+        hidden.hidden = !hidden.hidden
+    }, 1200)
 }
+
